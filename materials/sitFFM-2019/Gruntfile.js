2296f1bd5b6a1d3f432147d2556ad9eaf1076442
Volker Buzek <volker.buzek@js-soft.com>
2019-03-26 09:10:04 +0100

    feat(sitFFM): talk about UIveri5

---
+++ materials/sitFFM-2019/Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            src: {
+                options: {
+                    hostname: "sitFFM",
+                    base: "./",
+                    port: 2019,
+                    // keepalive: true
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    "**",
+                    "!node_modules/**",
+                    "!*.less"
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['connect:src', 'watch']);
+
+};
