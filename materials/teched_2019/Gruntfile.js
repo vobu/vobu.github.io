b5bf0373c5a2b6d83e89ca65b5f5b6601938dd55
Volker Buzek <volker.buzek@js-soft.com>
2019-10-03 14:27:46 +0200

    chore: initial commit TechEd 2019

---
+++ materials/teched_2019/Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        connect: {
+            src: {
+                options: {
+                    hostname: "teched",
+                    base: ["./", "../"],
+                    port: 1910,
+                    // keepalive: true
+                    // livereload: true
+                }
+            }
+        },
+        watch: {
+            // livereload: {
+            //     options: {
+            //         livereload: true
+            //     },
+            //     files: [
+            //         "**",
+            //         "!node_modules/**",
+            //         "!*.less"
+            //     ]
+            // },
+            styles: {
+                files: ['custom.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                  nospawn: true
+                }
+              }
+        },
+        less: {
+            development: {
+                files: {
+                  "custom.css": "custom.less" // destination file and source file
+                }
+              }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['connect:src', 'watch']);
+
+};
